14e3500bb52fdff8898b83870db6b50bd5541dcd
Samiul_Alom <samiulalom090@gmail.com>
2023-08-16 22:28:42 +0600

    modified:   src/components/pages/QuizPage.js

--- src/components/pages/QuizPage.js
+++ src/components/pages/QuizPage.js
@@ -3,7 +3,7 @@ import Rules from "./subpage/Ruels";
 
 export default function QuizPage() {
   return (
-    <div className="max-sm:p-10">
+    <div className="bg-white max-sm:p-10">
       <Rules />
     </div>
   );
