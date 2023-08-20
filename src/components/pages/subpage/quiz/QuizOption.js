91357731b2f4c9610ad0e222f7d3543254b9088e
shz-code <web.shahidul.alam@gmail.com>
2023-08-20 23:48:29 +0600

    v0.6.8.2

--- src/components/pages/subpage/quiz/QuizOption.js
+++ src/components/pages/subpage/quiz/QuizOption.js
@@ -12,22 +12,12 @@ export default function QuizOption({ option, ind, quizNumber }) {
     handleQuizUpdate(quizNumber, ind, !selected);
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter" || e.key === " ") {
-      // prevent default behavior
-      e.preventDefault();
-      // execute the click handler
-      setSelected((prev) => !prev);
-      handleQuizUpdate(quizNumber, ind, !selected);
-    }
-  };
   return (
     <div className="quizOption">
       <label
         htmlFor={`ck_${ind}_${quizNumber}`}
         className="cursor-pointer w-full bg-violet-600 text-white hover:bg-purple-800 rounded-md px-4 py-2
         flex items-center gap-2"
-        onKeyDown={handleKeyDown}
         tabIndex="0"
         aria-label={title}
       >
@@ -39,7 +29,7 @@ export default function QuizOption({ option, ind, quizNumber }) {
         <input
           type="radio"
           id={`ck_${ind}_${quizNumber}`}
-          selected={selected}
+          defaultChecked={selected}
           name={quizNumber}
           onChange={handleChange}
         />
